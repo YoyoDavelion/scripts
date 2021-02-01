803a8c80fab19826c59ec05574b68f0e1bd527c0
YoyoDavelion <76537650+YoyoDavelion@users.noreply.github.com>
2021-02-01 18:02:55 +0000

    Add files via upload

---
+++ extracthashes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Give the script execution perms with chmod +x exctracthashes.sh
+# Then run it with sudo ./extracthashes.sh or sudo sh extracthashes.sh
+if ! [ $(id -u) = 0 ]; then
+	echo "Run it with sudo"
+	exit 1
+fi
+$(rm -r extractedhashes 2>/dev/null)
+length=0
+for i in $(cat /etc/shadow | cut -d ":" -f 1,2); do 
+	length=${#i}
+	if [ $length -gt 20 ]; then 
+		echo "Hash extracted: $i"
+		$(echo $i >> extractedhashes)
+	fi
+done
+echo "Hashes has been exported to the file extractedhashes"
+#Script by YoyoDavelion
